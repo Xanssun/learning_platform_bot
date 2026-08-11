0c0d0b1f35b8e26d2045fdff15cc5c06af5c1764
Xanssun <Xanssun@yandex.ru>
2026-08-11 17:17:32 +0500

    minor fix

--- src/presentation/v1/routers/start.py
+++ src/presentation/v1/routers/start.py
@@ -1,23 +1,21 @@
-import structlog
-from aiogram.types import Message
+from aiogram.types import Message, User
 from dishka.integrations.aiogram import FromDishka, inject
 
 from src.application.common.interfaces.request_bus import RequestBus
 from src.application.v1.results import StartResult
 from src.application.v1.usecases import StartRequest
 
-log = structlog.get_logger(__name__)
-
 
 @inject
 async def start_router(
     msg: Message,
+    user: User,
     request_bus: FromDishka[RequestBus],
 ) -> None:
     result: StartResult = await request_bus.send(
         StartRequest(
-            telegram_user_id=msg.from_user.id,  # type: ignore[union-attr]
-            username=msg.from_user.username,  # type: ignore[union-attr]
+            telegram_user_id=user.id,
+            username=user.username,
         )
     )
 
